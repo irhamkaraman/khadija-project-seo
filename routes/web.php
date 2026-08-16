cd387bf2cadd8cd9142659f48652f88b9d5ce59c
irhamkaraman <irhamkaraman@gmail.com>
2026-08-16 14:36:57 +0700

    feat: add storage link route

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/storage-link', function () {
+    $targetFolder = base_path() . '/storage/app/public';
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
+
+    if (!file_exists($linkFolder)) {
+        symlink($targetFolder, $linkFolder);
+        return redirect()->back()->with('success', 'Penyimpanan di server sudah diaktifkan!');
+    }
+    else {
+        return redirect()->back()->with('error', 'Penyimpanan di server telah tersedia!');
+    }
+})->name('storage-link');
+
 Route::get('/{slug}', \App\Http\Controllers\SiteRedirectController::class);
