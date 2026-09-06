8e9b161a1aeeef987b0889c6abf2469c65a596e5
irhamkaraman <irhamkaraman@gmail.com>
2026-09-06 18:43:21 +0700

    fix: add cache-busting to favicon

--- resources/views/blog/layout.blade.php
+++ resources/views/blog/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}?v={{ filemtime(public_path('favicon.png')) }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}?v={{ filemtime(public_path('favicon.png')) }}">
 
     {{-- ====== ESSENTIAL OPEN GRAPH & WHATSAPP PREVIEW (TOP OF HEAD) ====== --}}
     @php
